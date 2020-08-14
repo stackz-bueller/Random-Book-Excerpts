32317be8484e0b699c0da4ffd806cb387d3d86c6
stackzbueller <ian.t.davis@outlook.com>
2020-08-14 18:13:53 +0000

    Switched to Production WSGI Server

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from flask import Flask, jsonify
 from threading import Thread
 from flask_restful import Resource, Api
+from gevent.pywsgi import WSGIServer
+
 import random_exc as exc
 
 app = Flask('')
 api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return "<h1>Random Book Excerpts</h1><p>A prototype API for Random Excerpts.</p>"
 
 class Info(Resource):
     def get(self):
         random = exc.runner()
         return jsonify(random)
 
-class Home(Resource):
-    def get(self):
-        return "<h1>Random Book Excerpts</h1><p>A prototype API for Random Excerpts.</p>"
-
 #creating api endpoint
 api.add_resource(Home, '/')
 api.add_resource(Info, '/api/restful')
 
 def run():
-    app.run(host='0.0.0.0',port=7210)
+    http_server = WSGIServer(('', 7210), app)
+    http_server.serve_forever()
 
 t = Thread(target=run)
 t.start()
